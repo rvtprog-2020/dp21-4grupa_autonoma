c1dcc71e5fc96a18de59abcf88c7f8e6cd92b14c
Vasiļenko Pāvels <paxka78@gmail.com>
2020-11-30 09:58:00 +0200

    Latest

---
+++ custom.js
@@ -0,0 +1,13 @@
+window.addEventListener('load',function(){
+    
+})
+
+if (parole = "admin") {
+    if (lietotajs = "admin") {
+        pageRedirect()
+    }
+}
+
+function pageRedirect(){
+    window.location.replace("admin.html")
+}
