7a8ae89306725aaf6229905ad173f9aa1c38a7b0
diana-rvt <70627950+diana-rvt@users.noreply.github.com>
2020-12-14 11:08:48 +0200

    izmaiņas

--- static/js/getData-admin.js
+++ static/js/getData-admin.js
@@ -14,13 +14,13 @@ async function getPosts(){
         /* Tas norada automobiļa statusu */
 
         if (post.buy == true) {
-            post.buy = "In Stock!"
+            post.buy = "Pieejams!"
         } else {
-            post.buy = "Sold out!"
+            post.buy = "Izīrēts!"
         }
             /* Tas ir daļa kur admins var pievienot vai nodzest automašinu. */
             
-            postsHTML = `<li><div class="border"><img class="image" src="${post.img}"><h2><b>Marka: </b>${post.marka}</h2><h2><b>Modelis: </b>${post.model}</h2><h2><b>Gads: </b>${post.year}</h2><h2><b>Nomas punkts: </b>${post.place}</h2><h1>${post.buy}</h1><div class="button"><a href="/panel/buy/${post.id}">Who customer?</a><a href="/panel/delete/${post.id}">Delete</a></div></div></li>`
+            postsHTML = `<li><div class="border"><img class="image" src="${post.img}"><h2><b>Marka: </b>${post.marka}</h2><h2><b>Modelis: </b>${post.model}</h2><h2><b>Gads: </b>${post.year}</h2><h2><b>Nomas punkts: </b>${post.place}</h2><h1>${post.buy}</h1><div class="button"><a href="/panel/buy/${post.id}">Kas izīrēja?</a><a href="/panel/delete/${post.id}">Dzēst</a></div></div></li>`
             posts.innerHTML = posts.innerHTML + postsHTML
     }
 }
--- static/js/getData-user.js
+++ static/js/getData-user.js
@@ -16,7 +16,7 @@ async function getPosts(){
         if (post.buy == false) {
             console.log("None")
         } else {
-            postsHTML = `<li><div class="border"><img class="image" src="${post.img}"><h2><b>Marka: </b>${post.marka}</h2><h2><b>Modelis: </b>${post.model}</h2><h2><b>Gads: </b>${post.year}</h2><h2><b>Nomas punkts: </b>${post.place}</h2><div class="button"><a href="/cars/buy/${post.id}">Buy now</a></div></div></li>`
+            postsHTML = `<li><div class="border"><img class="image" src="${post.img}"><h2><b>Marka: </b>${post.marka}</h2><h2><b>Modelis: </b>${post.model}</h2><h2><b>Gads: </b>${post.year}</h2><h2><b>Nomas punkts: </b>${post.place}</h2><div class="button"><a href="/cars/buy/${post.id}">Izīrēt</a></div></div></li>`
             posts.innerHTML = posts.innerHTML + postsHTML
         }
     }
